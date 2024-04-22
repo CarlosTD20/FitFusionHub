7722131f13336c9ee2078e8162d0dbb58281e5a9
Carlos Tornero <carlostornero204@gmail.com>
2024-04-22 20:26:45 +0200

    [Front] Nuevas rutas para la parte de músculos

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,12 @@ import DetailExercise from "./Components/ExercisesCompoenents/ExerciseDetailComp
 import ListExercises from "./Components/ExercisesCompoenents/ExerciseListComponent"
 import UpdateExercise from "./Components/ExercisesCompoenents/ExerciseUpdateComponent"
 import DeleteExercise from "./Components/ExercisesCompoenents/ExerciseDeleteComponent"
+import InsertExercise from "./Components/ExercisesCompoenents/ExerciseInsertComponent"
+import MusclesDetail from "./Components/MusclesComponents/MusclesDetailComponent"
+import MusclesInsert from "./Components/MusclesComponents/MusclesInsertComponent"
+import MusclesUpdate from "./Components/MusclesComponents/MusclesUpdateComponent"
+import MusclesDelete from "./Components/MusclesComponents/MusclesDeleteComponent"
+import MusclesList from "./Components/MusclesComponents/MusclesListComponet"
 
 
 export default function App() {
@@ -12,14 +18,19 @@ export default function App() {
     <>
       <BrowserRouter>
         <Header />
-        <div className="bg-gray-200 h-screen p-0 m-0">
+        <div>
           <Routes>
             <Route path='/' />
             <Route path='/exercises' element={<ListExercises />} />
-            <Route path='/exercises/:id' element={<DetailExercise />}/>
-            <Route path='/exercises/update/:id' element={<UpdateExercise />}/>
-            <Route path='/exercises/delete/:id' element={<DeleteExercise />}/>
-            <Route path='/muscles' />
+            <Route path='/exercises/:id' element={<DetailExercise />} />
+            <Route path='/exercises/insert' element={<InsertExercise />} />
+            <Route path='/exercises/update/:id' element={<UpdateExercise />} />
+            <Route path='/exercises/delete/:id' element={<DeleteExercise />} />
+            <Route path='/muscles' element={<MusclesList />} />
+            <Route path='/muscles/:id' element={<MusclesDetail />} />
+            <Route path='/muscles/insert' element={<MusclesInsert />} />
+            <Route path='/muscles/update/:id' element={<MusclesUpdate />} />
+            <Route path='/muscles/delete/:id' element={<MusclesDelete />} />
             <Route path='/' />
           </Routes>
         </div>

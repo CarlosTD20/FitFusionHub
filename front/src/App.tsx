965397dbba3b1d9d5e5cfaec3ee9e5d9b0184fff
Carlos Tornero <carlostornero204@gmail.com>
2024-04-29 23:34:20 +0200

    [Front] Rutas refactorizadas, exrtraidas al componente Routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Header from "./Components/Layout/Header"
-import DetailExercise from "./Components/ExercisesCompoenents/ExerciseDetailComponent"
-import ListExercises from "./Components/ExercisesCompoenents/ExerciseListComponent"
-import UpdateExercise from "./Components/ExercisesCompoenents/ExerciseUpdateComponent"
-import DeleteExercise from "./Components/ExercisesCompoenents/ExerciseDeleteComponent"
-import InsertExercise from "./Components/ExercisesCompoenents/ExerciseInsertComponent"
-import MusclesDetail from "./Components/MusclesComponents/MusclesDetailComponent"
-import MusclesInsert from "./Components/MusclesComponents/MusclesInsertComponent"
-import MusclesUpdate from "./Components/MusclesComponents/MusclesUpdateComponent"
-import MusclesDelete from "./Components/MusclesComponents/MusclesDeleteComponent"
-import MusclesList from "./Components/MusclesComponents/MusclesListComponet"
-
+import { BrowserRouter } from "react-router-dom"
+import Header from "./pages/Layout/Header"
+import RoutingComponent from "./pages/UIComponents/RoutingComponent"
 
 export default function App() {
 
@@ -18,22 +8,8 @@ export default function App() {
     <>
       <BrowserRouter>
         <Header />
-        <div>
-          <Routes>
-            <Route path='/' />
-            <Route path='/exercises' element={<ListExercises />} />
-            <Route path='/exercises/:id' element={<DetailExercise />} />
-            <Route path='/exercises/insert' element={<InsertExercise />} />
-            <Route path='/exercises/update/:id' element={<UpdateExercise />} />
-            <Route path='/exercises/delete/:id' element={<DeleteExercise />} />
-            <Route path='/muscles' element={<MusclesList />} />
-            <Route path='/muscles/:id' element={<MusclesDetail />} />
-            <Route path='/muscles/insert' element={<MusclesInsert />} />
-            <Route path='/muscles/update/:id' element={<MusclesUpdate />} />
-            <Route path='/muscles/delete/:id' element={<MusclesDelete />} />
-            <Route path='/' />
-          </Routes>
-        </div>
+        <RoutingComponent />
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   )
